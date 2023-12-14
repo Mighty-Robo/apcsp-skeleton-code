21cd4c77841599bf8f969ffdc0977216f8d843c5
Mighty-Robo <144164831+Mighty-Robo@users.noreply.github.com>
2023-12-14 19:23:06 +0000

    Update river-problem.py

--- river-problem.py
+++ river-problem.py
@@ -1,40 +1,37 @@
 import copy
 
+
 # Define a function that takes in a state as a dictionary and returns True if the state meets the conditions and False if it does not
 def isValid(state):
-    
+if state["wolf"] == state["goat"] and state["wolf"] != state["person"]
+return False
+elif state["goat"] == state["cabbage"] and state["goat"] != state["person"]
+return False
+else:
+return True
+
+
+
 
 # Define a function that takes in a state as a dictionary and returns a list of all valid states that can be reached from 1 move of the input state
 # This function will need to call the function isValid(state)
 def get_next_states(state):
 
 
-# Define a recursive function that takes in a current_state and win_state and returns the path to those states using the Depth First Search algorithm
-# This function will need to call the function get_next_states(state), as well as itself
-def dfs(current_state, win_state):
-    
-    
-# Test your code! Does it solve the river crossing riddle?
-initial_state = {
-    "wolf": False,
-    "goat": False,
-    "cabbage": False,
-    "person": False
-}
+next_states = []
 
-win_state = {
-    "wolf": True,
-    "goat": True,
-    "cabbage": True,
-    "person": True
-}
 
-visited_states = [initial_state]
-path = []
+same_side = []
 
-if dfs(initial_state, win_state):
-    for index, step in enumerate(path):
-        print("After move", index+1, "the state is ", step)
-else:
-    print("No solution found.")
 
+for thing in state:
+if state["person"] == state thing != "person":
+same_side.append(thing)
+for thing in same_side:
+temp_state = copy.deepcopy(state)
+temp_state[thing] = not state[thing]
+temp_state["person"] = not state["person"]
+
+
+if(isValid(temp_state) == True):
+next_states.append(temp_state)
